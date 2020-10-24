3f7b75825832e87c7d1cb1b611b8ee9c4422d2f5
reyza98 <70235414+reyza98@users.noreply.github.com>
2020-10-24 16:43:21 +0700

    Add files via upload

---
+++ fan2.py
@@ -0,0 +1,19 @@
+import requests
+import time
+from requests.exceptions import Timeout
+a = list(range(210150001,210300000))
+i=1
+for tokens in a:
+	try:
+		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 1cea6e797ac5ab1e27f25a449ad7aa1b81facd3f'}
+		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
+		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk'},timeout=1)
+		print(response)
+		print(response2)
+		print(i)
+		i+=1
+		print(tokens)
+	except Timeout:
+		print('ke '+str(tokens)+' gagal')
+	except:
+		print('error internet')
