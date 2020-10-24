1a669a68f3d8d2f77452225a667abbbaeac0278b
reyza98 <70235414+reyza98@users.noreply.github.com>
2020-10-24 16:46:03 +0700

    Update fan10.py

--- fan10.py
+++ fan10.py
@@ -5,9 +5,9 @@ a = list(range(211350001,211500000))
 i=1
 for tokens in a:
 	try:
-		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 1cea6e797ac5ab1e27f25a449ad7aa1b81facd3f'}
+		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token be2c85854c5b7c1950ed019624d48bb04a34760c'}
 		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
-		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Fan back dong kk'},timeout=1)
+		response2 = requests.post('http://id-api.spooncast.net/users/'+str(tokens)+'/fanmessages/',headers=headers,json={'contents':'Acaa ga bisa move on sama Yora, bantuin move on dengan cara fanback makasih'},timeout=1)
 		print(response)
 		print(response2)
 		print(i)
